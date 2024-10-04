773d77c27c27bdda00649e39b5bfd16be9ad2b71
SatyanarayanPrashar <satyanarayanprashar73@gmail.com>
2024-10-04 22:30:17 +0530

    resource setted up

--- src/app/api/resources/route.ts
+++ src/app/api/resources/route.ts
@@ -2,6 +2,7 @@
 
 import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
+import { Resource } from "@/types/resource";
 
 const prisma = new PrismaClient();
 
@@ -24,3 +25,26 @@ export async function POST(request: Request) {
   }
 }
 
+export async function GET() {
+  try {
+    const resources = await prisma.resource.findMany();
+    return NextResponse.json(resources, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching resources:", error);
+    return NextResponse.json({ error: "Error fetching resources" }, { status: 500 });
+  }
+}
+
+export const getResourceById = async (resourceId: string): Promise<Resource | null> => {
+  try {
+    const resource = await prisma.resource.findUnique({
+      where: {
+        id: resourceId,
+      },
+    });
+    return resource || null; // Return null if not found
+  } catch (error) {
+    console.error("Error fetching resource:", error);
+    return null; // Handle the error as appropriate
+  }
+};
