a260762dd0f2a0235027ebeea7c32281b7dfd7fc
SatyanarayanPrashar <satyanarayanprashar73@gmail.com>
2024-10-09 13:08:59 +0530

    verify email (commented out for dev)

---
+++ prisma/migrations/20241009030727_init/migration.sql
@@ -0,0 +1,35 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `expires` on the `VerificationToken` table. All the data in the column will be lost.
+  - You are about to drop the column `identifier` on the `VerificationToken` table. All the data in the column will be lost.
+  - You are about to drop the `Account` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Session` table. If the table is not empty, all the data it contains will be lost.
+  - Added the required column `userId` to the `VerificationToken` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Account" DROP CONSTRAINT "Account_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Session" DROP CONSTRAINT "Session_userId_fkey";
+
+-- DropIndex
+DROP INDEX "VerificationToken_identifier_token_key";
+
+-- AlterTable
+ALTER TABLE "VerificationToken" DROP COLUMN "expires",
+DROP COLUMN "identifier",
+ADD COLUMN     "activatedAt" TIMESTAMP(3),
+ADD COLUMN     "id" SERIAL NOT NULL,
+ADD COLUMN     "userId" INTEGER NOT NULL,
+ADD CONSTRAINT "VerificationToken_pkey" PRIMARY KEY ("id");
+
+-- DropTable
+DROP TABLE "Account";
+
+-- DropTable
+DROP TABLE "Session";
+
+-- AddForeignKey
+ALTER TABLE "VerificationToken" ADD CONSTRAINT "VerificationToken_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -1,8 +1,15 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
+import type { NextApiRequest, NextApiResponse } from 'next'
 
-import bcrypt from 'bcryptjs';
-import { prisma } from '@/lib/prisma';
-import { NextResponse } from 'next/server';
+import bcrypt from 'bcryptjs'
+import { prisma } from '@/lib/prisma'
+import { NextResponse } from 'next/server'
+import { randomUUID } from 'crypto'
+
+import Mailgun from 'mailgun.js'
+import formData from 'form-data'
+
+const API_KEY = process.env.MAILGUN_API || ''
+const DOMAIN = process.env.MAILGUN_DOMAIN || ''
 
 export async function POST(req: Request, res: Response){
   const { fullname, email, password } = await req.json()
@@ -15,6 +22,26 @@ export async function POST(req: Request, res: Response){
       password: hashed
     }
   })
+
+  //send token to user
+  // const token = await prisma.verificationToken.create({
+  //   data: {
+  //     token: `${randomUUID()}${randomUUID()}`.replace(/-/g,''),
+  //     userId: user.id
+  //   }
+  // })
+
+  // const mailgun = new Mailgun(formData)
+  // const client = mailgun.client({ username: 'api', key:  API_KEY})
+
+  // const messageData = {
+  //   from: `Example Email <hello@your-domain.com>`,
+  //   to: user.email,
+  //   subject: 'Please Activate Your Account',
+  //   text: `Hello ${user.name}, please activate your account by clicking this link: http://localhost:3000/auth/activate/${token.token}`,
+  // }
+
+  // await client.messages.create(DOMAIN, messageData)
 
   return NextResponse.json({
     user:{
